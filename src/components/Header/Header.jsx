1e13cf0164f79d66df84f494fae413f206d32430
levilu800b <levilu800b@gmail.com>
2023-04-07 21:58:48 +0100

    logo import

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { images } from '../../constants';
 
 const Header = () => {
-	return <div>Header</div>;
+	return <section>
+		<header>
+			<div>
+				<img src={images.Logo} alt="" />
+			</div>
+			<div></div>
+		</header>
+	</section>;
 };
 
 export default Header;
