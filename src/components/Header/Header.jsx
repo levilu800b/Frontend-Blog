fccfafe37a6a24e33d6a4d4ffa9573ab71bfec13
levilu800b <levilu800b@gmail.com>
2023-04-09 22:09:55 +0100

    Header set

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,33 +1,53 @@
 import React from 'react';
 import { images } from '../../constants/index';
 
+const NavItemsInfo = [
+	{
+		name: 'Home',
+	},
+	{
+		name: 'Articles',
+	},
+	{
+		name: 'Pages',
+	},
+	{
+		name: 'Pricing',
+	},
+	{
+		name: 'Faq',
+	},
+];
+
+const NavItem = ({name}) => {
+	return (
+		<li className="relative group">
+			<a href="/" className="px-4 py-2">
+				{name}
+			</a>
+			<span className="text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">
+				/
+			</span>
+		</li>
+	)
+}
+
 const Header = () => {
 	return (
 		<section>
-			<header className='container mx-auto px-5 flex justify-between py-4 items-center'>
+			<header className="container mx-auto px-5 flex justify-between py-4 items-center">
 				<div>
 					<img src={images.logo} alt="logo" />
 				</div>
-				<div className='flex gap-x-9 items-center'>
-					<ul className='flex gap-x-5 font-semibold'>
-						<li>
-							<a href="/">Home</a>
-							<span>/</span>
-						</li>
-						<li>
-							<a href="/">Articles</a>
-						</li>
-						<li>
-							<a href="/">Pages</a>
-						</li>
-						<li>
-							<a href="/">Pricing</a>
-						</li>
-						<li>
-							<a href="/">Faq</a>
-						</li>
+				<div className="flex gap-x-9 items-center">
+					<ul className="flex gap-x-2 font-semibold">
+						{NavItemsInfo.map((item, index) => (
+							<NavItem key={index} name={item.name} />
+						))}
 					</ul>
-					<button className='border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300'>Sign in</button>
+					<button className="border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300">
+						Sign in
+					</button>
 				</div>
 			</header>
 		</section>
