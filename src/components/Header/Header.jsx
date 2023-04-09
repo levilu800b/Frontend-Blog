61711a53c9cd535aef8f52e688d906ca1af834da
levilu800b <levilu800b@gmail.com>
2023-04-09 20:13:55 +0100

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,14 +2,36 @@ import React from 'react';
 import { images } from '../../constants/index';
 
 const Header = () => {
-	return <section>
-		<header>
-			<div>
-				<img src={images.logo} alt="logo" />
-			</div>
-			<div></div>
-		</header>
-	</section>;
+	return (
+		<section>
+			<header className='container mx-auto px-5 flex justify-between py-4 items-center'>
+				<div>
+					<img src={images.logo} alt="logo" />
+				</div>
+				<div className='flex gap-x-9 items-center'>
+					<ul className='flex gap-x-5 font-semibold'>
+						<li>
+							<a href="/">Home</a>
+							<span>/</span>
+						</li>
+						<li>
+							<a href="/">Articles</a>
+						</li>
+						<li>
+							<a href="/">Pages</a>
+						</li>
+						<li>
+							<a href="/">Pricing</a>
+						</li>
+						<li>
+							<a href="/">Faq</a>
+						</li>
+					</ul>
+					<button className='border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300'>Sign in</button>
+				</div>
+			</header>
+		</section>
+	);
 };
 
 export default Header;
