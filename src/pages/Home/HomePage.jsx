b5063901205d28e33a4be1c439f28abea39e23d8
levilu800b <levilu800b@gmail.com>
2023-04-18 10:39:33 +0100

    CTA wave svg

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+
 import MainLayout from '../../components/MainLayout/MainLayout';
 import Hero from './container/Hero';
+import CTA from './container/CTA';
 import Articles from './container/Articles';
 
 const HomePage = () => {
 	return <MainLayout>
 		<Hero />
 		<Articles />
+		<CTA />
 	</MainLayout>;
 };
 
--- src/pages/Home/container/CTA.jsx
+++ src/pages/Home/container/CTA.jsx
@@ -4,8 +4,8 @@ const CTA = () => {
   return (
     <>
     <svg
-    width="2160"
-    height="263"
+    className='w-full h-auto max-h-40'
+    preserveAspectRatio='none'
     viewBox="0 0 2160 263"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
@@ -15,7 +15,7 @@ const CTA = () => {
         fill-rule="evenodd"
         clip-rule="evenodd"
         d="M2160 262.5H0V0C360 80 720 120 1080 120C1440 120 1800 80 2160 0V262.5Z"
-        fill="#F9FAFB"
+        fill="#5800FF"
     />
     </svg>
 
