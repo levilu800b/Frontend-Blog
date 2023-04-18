6f9eba4a0c901448bb43e9994e77118f27133175
levilu800b <levilu800b@gmail.com>
2023-04-18 14:31:15 +0100

    CTA text and input

--- src/pages/Home/container/CTA.jsx
+++ src/pages/Home/container/CTA.jsx
@@ -4,7 +4,7 @@ const CTA = () => {
   return (
     <>
     <svg
-    className='w-full h-auto max-h-40'
+    className='w-full h-auto max-h-40 translate-y-[1px]'
     preserveAspectRatio='none'
     viewBox="0 0 2160 263"
     fill="none"
@@ -19,7 +19,17 @@ const CTA = () => {
     />
     </svg>
 
-    <section></section>
+    <section className='relative bg-primary px-5'>
+        <div className='container grid grid-cols-12 mx-auto'>
+            <div className='col-span-12'>
+                <h2 className='text-white font-roboto font-bold text-2xl'>Get our stories delivered from us to your inbox weekly.</h2>
+                <div className='w-full max-w-[494px] mt-12 space-y-3 mx-auto'>
+                    <input type="text" className='px-4 py-4' placeholder='Your Email' />
+                    <button>Get started</button>
+                </div>
+            </div>
+        </div>
+    </section>
     </>
   )
 }
