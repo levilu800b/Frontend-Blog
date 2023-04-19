517e075782ac26dcedba1cf75a6cb1cbe13f9161
levilu800b <levilu800b@gmail.com>
2023-04-19 17:27:07 +0100

    Footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const Footer = () => {
-	return <div>Footer</div>;
+	return <section className='bg-dark-soft'>
+		<footer className='container mx-auto grid grid-cols-10'>
+			<div className='col-span-5'>
+				<h3 className='text-dark-light font-bold'>Product</h3>
+				<ul className='text-[#959EAD] text-sm mt-5 space-y-4'></ul>
+			</div>
+		</footer>
+	</section>;
 };
 
 export default Footer;
