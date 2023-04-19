1748ede09adadd881d3f1ea6031e57b1e60362f0
levilu800b <levilu800b@gmail.com>
2023-04-19 20:43:22 +0100

    Footer Tablet devices

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { AiOutlineTwitter, AiFillInstagram } from 'react-icons/ai';
 import { FaFacebook } from 'react-icons/fa';
 import { BsTelegram } from 'react-icons/bs';
+import { TbHeartHandshake } from 'react-icons/tb';
 
 
 
@@ -10,10 +11,10 @@ import { images } from '../../constants/index';
 const Footer = () => {
 	return (
 		<section className="bg-dark-soft">
-			<footer className="container mx-auto grid grid-cols-10 px-5 py-10 gap-y-10 gap-x-5">
-				<div className="col-span-5">
-					<h3 className="text-dark-light font-bold">Product</h3>
-					<ul className="text-[#959EAD] text-sm mt-5 space-y-4">
+			<footer className="container mx-auto grid grid-cols-10 px-5 py-10 gap-y-10 gap-x-5 md:pt-20 md:grid-cols-12">
+				<div className="col-span-5 md:col-span-4">
+					<h3 className="text-dark-light font-bold md:text-lg">Product</h3>
+					<ul className="text-[#959EAD] text-sm mt-5 space-y-4 md:text-base">
 						<li>
 							<a href="/">Landing Page</a>
 						</li>
@@ -31,9 +32,9 @@ const Footer = () => {
 						</li>
 					</ul>
 				</div>
-				<div className="col-span-5">
-					<h3 className="text-dark-light font-bold">Services</h3>
-					<ul className="text-[#959EAD] text-sm mt-5 space-y-4">
+				<div className="col-span-5 md:col-span-4">
+					<h3 className="text-dark-light font-bold md:text-lg">Services</h3>
+					<ul className="text-[#959EAD] text-sm mt-5 space-y-4 md:text-base">
 						<li>
 							<a href="/">Documentation</a>
 						</li>
@@ -51,9 +52,9 @@ const Footer = () => {
 						</li>
 					</ul>
 				</div>
-				<div className="col-span-5">
-					<h3 className="text-dark-light font-bold">Company</h3>
-					<ul className="text-[#959EAD] text-sm mt-5 space-y-4">
+				<div className="col-span-5 md:col-span-4 md:col-start-5">
+					<h3 className="text-dark-light font-bold md:text-lg">Company</h3>
+					<ul className="text-[#959EAD] text-sm mt-5 space-y-4 md:text-base">
 						<li>
 							<a href="/">About</a>
 						</li>
@@ -68,9 +69,9 @@ const Footer = () => {
 						</li>
 					</ul>
 				</div>
-				<div className="col-span-5">
-					<h3 className="text-dark-light font-bold">More</h3>
-					<ul className="text-[#959EAD] text-sm mt-5 space-y-4">
+				<div className="col-span-5 md:col-span-4">
+					<h3 className="text-dark-light font-bold md:text-lg">More</h3>
+					<ul className="text-[#959EAD] text-sm mt-5 space-y-4 md:text-base">
 						<li>
 							<a href="/">Documentation</a>
 						</li>
@@ -82,31 +83,45 @@ const Footer = () => {
 						</li>
 					</ul>
 				</div>
-				<div className='col-span-10'>
-					<img src={images.logo} alt="logo" className='brightness-0 invert mx-auto' />
-					<p className='text-sm text-dark-light text-center mt-4'>Build a modern and creative website with crealand</p>
-					<ul className="flex justify-center items-center mt-5 space-x-4 text-gray-300">
+				<div className="col-span-10 md:order-first md:col-span-4">
+					<img
+						src={images.logo}
+						alt="logo"
+						className="brightness-0 invert mx-auto md:mx-0"
+					/>
+					<p className="text-sm text-dark-light text-center mt-4 md:text-left md:text-base">
+						Build a modern and creative website with us
+					</p>
+					<ul className="flex justify-center items-center mt-5 space-x-4 text-gray-300 md:justify-start">
 						<li>
 							<a href="/">
-								<AiOutlineTwitter className='w-6 h-auto' />
+								<AiOutlineTwitter className="w-6 h-auto" />
 							</a>
 						</li>
 						<li>
 							<a href="/">
-								<AiFillInstagram className='w-6 h-auto' />
+								<AiFillInstagram className="w-6 h-auto" />
 							</a>
 						</li>
 						<li>
 							<a href="/">
-								<FaFacebook className='w-6 h-auto' />
+								<FaFacebook className="w-6 h-auto" />
 							</a>
 						</li>
 						<li>
 							<a href="/">
-								<BsTelegram className='w-6 h-auto' />
+								<BsTelegram className="w-6 h-auto" />
 							</a>
 						</li>
 					</ul>
+				</div>
+				<div className="hidden md:flex flex-col items-center space-y-4 md:col-span-12">
+					<div className="bg-primary text-white p-3 rounded-full">
+						<TbHeartHandshake className="w-7 h-auto" />
+					</div>
+					<p className="font-bold italic text-dark-light">
+						Copyright © 2023. Crafted with love.
+					</p>
 				</div>
 			</footer>
 		</section>
