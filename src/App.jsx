ddd3f3c90faae2c4305111062df09b00e37cc165
levilu800b <levilu800b@gmail.com>
2023-04-21 14:00:13 +0100

    BreadCrumbs Link

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/Home/HomePage';
+import ArticleDetailPage from './pages/ArticleDetail/ArticleDetailPage';
 
 function App() {
   return (
 		<div className="App font-opensans">
 			<Routes>
 				<Route index path="/" element={<HomePage />} />
-				{/* <Route index path="/blog/:id" element={<HomePage />} /> */}
+				<Route index path="/blog/:id" element={<ArticleDetailPage />} />
 			</Routes>
 		</div>
 	);
---
+++ src/pages/ArticleDetail/ArticleDetailPage.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import MainLayout from '../../components/MainLayout/MainLayout';
+import BreadCrumbs from '../../components/BreadCrumbs/BreadCrumbs';
+
+const breadCrumbsData = [
+	{ name: 'Home', link: '/' },
+	{ name: 'Blog', link: '/blog' },
+	{ name: 'Article title', link: '/blog/1' },
+];
+
+const ArticleDetailPage = () => {
+	return (
+		<MainLayout>
+			<section className="container mx-auto max-w-5xl flex flex-col px-5 py-5">
+				<article className="flex-1">
+					<BreadCrumbs data={breadCrumbsData}/>
+				</article>
+			</section>
+		</MainLayout>
+	);
+};
+
+export default ArticleDetailPage;
