09e0aec4db6dcf58df3aace0e87413199f0dca3b
levilu800b <levilu800b@gmail.com>
2023-04-21 13:36:30 +0100

    Added routes and route for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
 import HomePage from './pages/Home/HomePage';
 
 function App() {
   return (
-    <div className="App font-opensans">
-      <HomePage />
-    </div>
-  );
+		<div className="App font-opensans">
+			<Routes>
+				<Route index path="/" element={<HomePage />} />
+				{/* <Route index path="/blog/:id" element={<HomePage />} /> */}
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
