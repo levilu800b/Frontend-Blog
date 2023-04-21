cd5b0c5a051a435fb586c29c04195f552c6044cd
levilu800b <levilu800b@gmail.com>
2023-04-21 16:37:41 +0100

    Suggested posts

--- src/pages/ArticleDetail/ArticleDetailPage.jsx
+++ src/pages/ArticleDetail/ArticleDetailPage.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import MainLayout from '../../components/MainLayout/MainLayout';
 import BreadCrumbs from '../../components/BreadCrumbs/BreadCrumbs';
+import { images } from '../../constants/index';
+import { Link } from 'react-router-dom';
+import SuggestedPosts from './container/SuggestedPosts';
 
 const breadCrumbsData = [
 	{ name: 'Home', link: '/' },
@@ -8,13 +11,66 @@ const breadCrumbsData = [
 	{ name: 'Article title', link: '/blog/1' },
 ];
 
+const postsData = [
+    {
+    _id: "1",
+    image: images.PostImage,
+    title: "Help children get better education",
+    createdAt: "2023-01-28T15:35:53.607+0000",
+  },
+  {
+    _id: "2",
+    image: images.PostImage,
+    title: "Help children get better education",
+    createdAt: "2023-01-28T15:35:53.607+0000",
+  },
+  {
+    _id: "3",
+    image: images.PostImage,
+    title: "Help children get better education",
+    createdAt: "2023-01-28T15:35:53.607+0000",
+  },
+  {
+    _id: "4",
+    image: images.PostImage,
+    title: "Help children get better education",
+    createdAt: "2023-01-28T15:35:53.607+0000",
+  },
+]
+
 const ArticleDetailPage = () => {
 	return (
 		<MainLayout>
 			<section className="container mx-auto max-w-5xl flex flex-col px-5 py-5">
 				<article className="flex-1">
-					<BreadCrumbs data={breadCrumbsData}/>
+					<BreadCrumbs data={breadCrumbsData} />
+					<img
+						className="rounded-xl w-full"
+						src={images.PostImage}
+						alt="laptop"
+					/>
+					<Link
+						to="/blog?category=selectedCategory"
+						className="text-primary text-sm font-roboto inline-block mt-4"
+					>
+						EDUCATION
+					</Link>
+					<h1 className="text-xl font-medium font-roboto mt-4 text-dark-hard">
+						Youths needs to get into better Education
+					</h1>
+					<div className="mt-4 text-dark-soft">
+						<p className="leading-7">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua.
+							Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae
+							congue mauris rhoncus aenean vel elit scelerisque. In egestas erat
+							imperdiet sed euismod nisi porta lorem mollis. Morbi tristique
+							senectus et netus. Mattis pellentesque id nibh tortor id aliquet
+							lectus proin.
+						</p>
+					</div>
 				</article>
+                <SuggestedPosts header='Latest Article' posts={postsData} />
 			</section>
 		</MainLayout>
 	);
