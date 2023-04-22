27393d556eb34f589bb801a90fe86762bbf22693
levilu800b <levilu800b@gmail.com>
2023-04-22 19:54:08 +0100

    Add comment

--- src/components/Comments/CommentsContainer.jsx
+++ src/components/Comments/CommentsContainer.jsx
@@ -1,13 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+import { getCommentsData } from '../../data/comments';
 import CommentForm from './CommentForm'
 
 const CommentsContainer = ({ className }) => {
-    const addCommentHandler = (value, parent = null, replyOnUser = null) => {}
+    const [comments, setComments] = useState([]);
+
+    console.log(comments);
+
+    useEffect(() => {
+        (async() => {
+            const commentData = await getCommentsData();
+            setComments(commentData);
+        }) ()
+    }, [])
+
+    const addCommentHandler = (value, parent = null, replyOnUser = null) => {
+        const newComment = {
+					_id: '10',
+					user: {
+						_id: 'a',
+						name: 'Mohammad Rezaii',
+					},
+					desc: value,
+					post: '1',
+					parent: parent,
+					replyOnUser: replyOnUser,
+					createdAt: '2022-12-31T17:22:05.092+0000',
+				};
+        setComments((currentState) => {
+            return [newComment, ...currentState];
+        });
+    }
 
 
   return (
     <div className={`${className}`}>
-        <CommentForm btnLabel="Send" />
+        <CommentForm btnLabel="Send" formSubmitHandler={(value) => addCommentHandler(value)} />
     </div>
   )
 }
