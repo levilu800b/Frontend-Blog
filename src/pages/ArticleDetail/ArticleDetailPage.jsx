b2b34cae152373bb9f4e74ad66c1495314069076
levilu800b <levilu800b@gmail.com>
2023-04-23 15:10:56 +0100

    social share section

--- src/pages/ArticleDetail/ArticleDetailPage.jsx
+++ src/pages/ArticleDetail/ArticleDetailPage.jsx
@@ -80,14 +80,19 @@ const ArticleDetailPage = () => {
 							lectus proin.
 						</p>
 					</div>
-					<CommentsContainer className="mt-10" loggedInUserId='a' />
+					<CommentsContainer className="mt-10" loggedInUserId="a" />
 				</article>
-				<SuggestedPosts
-					header="Latest Article"
-					posts={postsData}
-					tags={tagsData}
-					className="mt-8 lg:mt-0 lg:max-w-xs"
-				/>
+				<div>
+					<SuggestedPosts
+						header="Latest Article"
+						posts={postsData}
+						tags={tagsData}
+						className="mt-8 lg:mt-0 lg:max-w-xs"
+					/>
+					<div className='mt-7'>
+						<h2 className='font-roboto font-medium text-dark-hard mb-4 md:text-xl'>Share on:</h2>
+					</div>
+				</div>
 			</section>
 		</MainLayout>
 	);
