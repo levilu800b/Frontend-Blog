1356a18741afb90535d5c002b6c10cfbb7138f95
levilu800b <levilu800b@gmail.com>
2023-04-26 18:54:05 +0100

    Form validation

--- src/pages/register/RegisterPage.jsx
+++ src/pages/register/RegisterPage.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import { useForm } from 'react-hook-form';
 import MainLayout from '../../components/MainLayout/MainLayout'
+import { Link } from 'react-router-dom';
 
 const RegisterPage = () => {
+    const {
+			register,
+			handleSubmit,
+			formState: { errors, isValid },
+			watch,
+		} = useForm({
+			defaultValues: {
+				name: '',
+				email: '',
+				password: '',
+				confirmPassword: '',
+			},
+            mode: 'onChange',
+		});
+
     const submitHandler = () => {}
 
   return (
@@ -11,7 +28,7 @@ const RegisterPage = () => {
 					<h1 className="font-roboto text-2xl font-bold text-center text-dark-hard mb-8">
 						Sign Up
 					</h1>
-					<form onSubmit={submitHandler}>
+					<form onSubmit={handleSubmit(submitHandler)}>
 						<div className="flex flex-col mb-6 w-full">
 							<label
 								htmlFor="name"
@@ -22,9 +39,22 @@ const RegisterPage = () => {
 							<input
 								type="text"
 								id="name"
+								{...register('name', {
+                                    minLength: {
+                                        value: 1,
+                                        message: 'Name length must be at least 1 character',
+                                    },
+                                    required: {
+                                        value: true,
+                                        message: 'Name is required',
+                                    }
+                                })}
 								placeholder="Enter name"
 								className="placeholder:text-[#959EAD] text-dark-hard mt-3 rounded-lg px-5 py-4 font-semibold block outline-none border border-[#C3CAD9]"
 							/>
+                            {errors.name?.message && (
+                                <p className="text-red-500 text-xs mt-1">{errors.name?.message}</p>
+                            )}
 						</div>
 						<div className="flex flex-col mb-6 w-full">
 							<label
@@ -36,6 +66,7 @@ const RegisterPage = () => {
 							<input
 								type="email"
 								id="email"
+								{...register('email')}
 								placeholder="Enter email"
 								className="placeholder:text-[#959EAD] text-dark-hard mt-3 rounded-lg px-5 py-4 font-semibold block outline-none border border-[#C3CAD9]"
 							/>
@@ -50,6 +81,7 @@ const RegisterPage = () => {
 							<input
 								type="password"
 								id="password"
+								{...register('password')}
 								placeholder="Enter password"
 								className="placeholder:text-[#959EAD] text-dark-hard mt-3 rounded-lg px-5 py-4 font-semibold block outline-none border border-[#C3CAD9]"
 							/>
@@ -64,10 +96,30 @@ const RegisterPage = () => {
 							<input
 								type="password"
 								id="confirmPassword"
+								{...register('confirmPassword')}
 								placeholder="Enter confirm password"
 								className="placeholder:text-[#959EAD] text-dark-hard mt-3 rounded-lg px-5 py-4 font-semibold block outline-none border border-[#C3CAD9]"
 							/>
 						</div>
+						<Link
+							to="/forgot-password"
+							className="text-sm font-semibold text-primary"
+						>
+							Forgot password
+						</Link>
+						<button
+							type="submit"
+							className="bg-primary text-white font-bold text-lg py-4 px-8 w-full rounded-lg my-6"
+						>
+							Register
+						</button>
+						<p className="text-sm font-semibold text-[#5A7184]">
+							You have an account?
+							<Link to="/login" className="text-primary font-bold">
+								{' '}
+								Login now
+							</Link>
+						</p>
 					</form>
 				</div>
 			</section>
