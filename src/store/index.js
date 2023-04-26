8a2d821c1e3d7b47b776ba93f9939613bfbae6af
levilu800b <levilu800b@gmail.com>
2023-04-26 17:39:37 +0100

    checking redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import { countReducer } from "./reducers/countReducers";
+
 const store = configureStore({
     reducer: {
-        
+        count: countReducer
     }
 })
 
