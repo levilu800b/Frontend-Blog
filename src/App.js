dfe9ab4b82d5331a3530164b67637a9fa3026a08
levilu800b <levilu800b@gmail.com>
2023-04-28 13:13:13 +0100

    added profile page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import HomePage from './pages/Home/HomePage';
 import ArticleDetailPage from './pages/ArticleDetail/ArticleDetailPage';
 import RegisterPage from './pages/register/RegisterPage';
 import LoginPage from './pages/login/LoginPage';
+import ProfilePage from './pages/profile/ProfilePage';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
 				<Route path="/blog/:id" element={<ArticleDetailPage />} />
 				<Route path="/register" element={<RegisterPage />} />
 				<Route path="/login" element={<LoginPage />} />
+				<Route path="/profile" element={<ProfilePage />} />
 			</Routes>
 			<Toaster />
 		</div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -128,41 +128,45 @@ const Header = () => {
 					</ul>
 					{userState.userInfo ? (
 						<div className="text-white items-center gap-y-5 lg:text-dark-soft flex flex-col lg:flex-row gap-x-2 font-semibold">
-							<div className='relative group'>
-							<div className="flex flex-col items-center">
-								<button
-									className="flex gap-x-1 items-center mt-5 lg:mt-0 border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300"
-									onClick={() => setProfileDropdown(!profileDropdown)}
-								>
-									<span>Profile</span>
-									<MdKeyboardArrowDown />
-								</button>
-								<div
-									className={`${
-										profileDropdown ? 'block' : 'hidden'
-									} lg:hidden transition-all duration-500 pt-4 lg:absolute lg:bottom-0 lg:right-0 lg:transform lg:translate-y-full lg:group-hover:block w-max`}
-								>
-									<ul className="bg-dark-soft lg:bg-transparent text-center flex flex-col shadow-lg rounded-lg overflow-hidden">
-										<button
-											type="button"
-											className="hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft"
-										>
-											Dashboard
-										</button>
-										<button
-											onClick={logoutHandler}
-											type="button"
-											className="hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft"
-										>
-											Logout
-										</button>
-									</ul>
+							<div className="relative group">
+								<div className="flex flex-col items-center">
+									<button
+										className="flex gap-x-1 items-center mt-5 lg:mt-0 border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300"
+										onClick={() => setProfileDropdown(!profileDropdown)}
+									>
+										<span>Account</span>
+										<MdKeyboardArrowDown />
+									</button>
+									<div
+										className={`${
+											profileDropdown ? 'block' : 'hidden'
+										} lg:hidden transition-all duration-500 pt-4 lg:absolute lg:bottom-0 lg:right-0 lg:transform lg:translate-y-full lg:group-hover:block w-max`}
+									>
+										<ul className="bg-dark-soft lg:bg-transparent text-center flex flex-col shadow-lg rounded-lg overflow-hidden">
+											<button
+												onClick={() => navigate('/profile')}
+												type="button"
+												className="hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft"
+											>
+												Profile
+											</button>
+											<button
+												onClick={logoutHandler}
+												type="button"
+												className="hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft"
+											>
+												Logout
+											</button>
+										</ul>
+									</div>
 								</div>
-							</div>
 							</div>
 						</div>
 					) : (
-						<button onClick={() => navigate('/login')} className="mt-5 lg:mt-0 border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300">
+						<button
+							onClick={() => navigate('/login')}
+							className="mt-5 lg:mt-0 border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300"
+						>
 							Sign in
 						</button>
 					)}
