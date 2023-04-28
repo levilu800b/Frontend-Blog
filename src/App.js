606f98d779ae9580bbe907af9231f9309fe03dd0
levilu800b <levilu800b@gmail.com>
2023-04-28 12:58:29 +0100

    login page with functionality

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 import HomePage from './pages/Home/HomePage';
 import ArticleDetailPage from './pages/ArticleDetail/ArticleDetailPage';
 import RegisterPage from './pages/register/RegisterPage';
+import LoginPage from './pages/login/LoginPage';
+
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
 				<Route index path="/" element={<HomePage />} />
 				<Route path="/blog/:id" element={<ArticleDetailPage />} />
 				<Route path="/register" element={<RegisterPage />} />
+				<Route path="/login" element={<LoginPage />} />
 			</Routes>
 			<Toaster />
 		</div>
