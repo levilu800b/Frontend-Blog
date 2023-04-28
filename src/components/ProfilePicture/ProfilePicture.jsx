0e9a0cce37498c449ed42b7a14e3e4c92ced7166
levilu800b <levilu800b@gmail.com>
2023-04-28 14:30:34 +0100

    profile picture icon and button

--- src/components/ProfilePicture/ProfilePicture.jsx
+++ src/components/ProfilePicture/ProfilePicture.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { HiOutlineCamera } from 'react-icons/hi';
+
+import { stables } from '../../constants/index';
 
 const ProfilePicture = ({ avatar }) => {
   return (
     <div className='w-full flex items-center gap-x-4'>
-        <div>
-            <label htmlFor="">
+        <div className='relative w-20 h-20 rounded-full outline outline-offset-2 outline-1 outline-primary overflow-hidden'>
+            <label htmlFor="profilePicture" className='cursor-pointer absolute inset-0 rounded-full bg-transparent'>
                 {avatar ? (
-                    <img src={avatar} alt="" />
+                    <img src={stables.UPLOAD_FOLDER_BASE_URL + avatar} alt="profile" />
+                ): (
+                    <div className='w-full h-full bg-blue-50/50 flex justify-center items-center'>
+                        <HiOutlineCamera className='w-7 h-auto text-primary' />
+                    </div>
                 )}
             </label>
-            <input type="file" className='sr-only' />
+            <input type="file" className='sr-only' id='profilePicture' />
         </div>
-        <button type='button'>Delete</button>
+        <button type='button' className='border border-red-500 rounded-lg px-4 py-2 text-red-500'>Delete</button>
     </div>
   )
 }
