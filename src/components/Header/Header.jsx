dd808fb511e30741c1cd5f70c4e183b148ac576b
levilu800b <levilu800b@gmail.com>
2023-04-30 13:59:14 +0100

    Nav fixes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 import { MdKeyboardArrowDown } from 'react-icons/md';
 import { useSelector, useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import { images } from '../../constants/index';
 import { logout } from '../../store/actions/user';
@@ -11,23 +11,30 @@ const NavItemsInfo = [
 	{
 		name: 'Home',
 		type: 'link',
+		href: '/',
 	},
 	{
 		name: 'Articles',
 		type: 'link',
+		href: '/articles',
 	},
 	{
 		name: 'Pages',
 		type: 'dropdown',
-		items: ['About Us', 'Contact Us'],
+		items: [
+			{ title: 'About Us', href: '/about' },
+			{ title: 'Contact Us', href: '/contact' },
+		],
 	},
 	{
 		name: 'Pricing',
 		type: 'link',
+		href: '/pricing',
 	},
 	{
 		name: 'Faq',
 		type: 'link',
+		href: '/faq',
 	},
 ];
 
@@ -44,9 +51,9 @@ const NavItem = ({ item }) => {
 		<li className="relative group">
 			{item.type === 'link' ? (
 				<>
-					<a href="/" className="px-4 py-2">
+					<Link to={item.href} className="px-4 py-2">
 						{item.name}
-					</a>
+					</Link>
 					<span className="cursor-pointer text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">
 						/
 					</span>
@@ -67,13 +74,13 @@ const NavItem = ({ item }) => {
 					>
 						<ul className="bg-dark-soft lg:bg-transparent text-center flex flex-col shadow-lg rounded-lg overflow-hidden">
 							{item.items.map((page, index) => (
-								<a
+								<Link
 									key={index}
-									href="/"
+									to={page.href}
 									className="hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft"
 								>
-									{page}
-								</a>
+									{page.title}
+								</Link>
 							))}
 						</ul>
 					</div>
@@ -103,9 +110,9 @@ const Header = () => {
 	return (
 		<section className="sticky top-0 left-0 right-0 z-50 bg-white">
 			<header className="container mx-auto px-5 flex justify-between py-4 items-center">
-				<div>
+				<Link to="/">
 					<img className="w-16" src={images.logo} alt="logo" />
-				</div>
+				</Link>
 				<div className="lg:hidden z-50">
 					{navIsVisible ? (
 						<AiOutlineClose
