2424a4e1036e9ca2b4efad2de991ce20fdfd0ac6
levilu800b <levilu800b@gmail.com>
2023-05-02 12:44:03 +0100

    show all post in the home page

--- src/components/ArticleCard/ArticleCard.jsx
+++ src/components/ArticleCard/ArticleCard.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import {BsCheckLg} from 'react-icons/bs';
+import { BsCheckLg } from 'react-icons/bs';
+import { AiOutlineClose } from 'react-icons/ai';
 
 import { images, stables } from '../../constants/index';
 
-const ArticleCard = ({ post, className}) => {
-  return (
+const ArticleCard = ({ post, className }) => {
+	return (
 		<div
 			className={`rounded-xl overflow-hidden shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px] ${className}`}
 		>
@@ -27,31 +28,46 @@ const ArticleCard = ({ post, className}) => {
 				<div className="flex justify-between flex-nowrap items-center mt-6">
 					<div className="flex items-center gap-x-2 md:gap-x-2.5">
 						<img
-							src={images.PostProfileImage}
-							alt=""
-							className="w-9 h-9 md:w-10 md:h-10"
+							src={
+								post.user.avatar
+									? stables.UPLOAD_FOLDER_BASE_URL + post.user.avatar
+									: images.userImage
+							}
+							alt="post profile"
+							className="w-9 h-9 md:w-10 md:h-10 rounded-full"
 						/>
 						<div className="flex flex-col">
 							<h4 className="font-bold italic text-dark-soft text-sm md:text-base">
-								Jasper Micheals
+								{post.user.name}
 							</h4>
 							<div className="flex items-center gap-x-2">
-								<span className="bg-[#36B37E] w-fit bg-opacity-20 p-1.5 rounded-full">
-									<BsCheckLg className="w-1.5 h-1.5 text-[#36B37E]" />
+								<span
+									className={`${
+										post.user.verified ? 'bg-[#36B37E]' : 'bg-red-500'
+									} w-fit bg-opacity-20 p-1.5 rounded-full`}
+								>
+									{post.user.verified ? (
+										<BsCheckLg className="w-1.5 h-1.5 text-[#36B37E]" />
+									) : (
+										<AiOutlineClose className="w-1.5 h-1.5 text-red-500" />
+									)}
 								</span>
 								<span className="italic text-dark-light text-xs md:text-sm">
-									Verified writer
+									{post.user.verified ? 'Verified' : 'Unverified'} writer
 								</span>
 							</div>
 						</div>
 					</div>
 					<span className="font-bold text-dark-light italic text-sm md:text-base">
-						02 May
+						{new Date(post.createdAt).getDate()}{' '}
+						{new Date(post.createdAt).toLocaleString('default', {
+							month: 'long',
+						})}
 					</span>
 				</div>
 			</div>
 		</div>
 	);
-}
+};
 
-export default ArticleCard
+export default ArticleCard;
--- src/constants/images.js
+++ src/constants/images.js
@@ -4,6 +4,7 @@ import PostImage from '../assets/Posts/post1.jpg';
 import PostProfileImage from '../assets/Posts/post-profile.svg';
 import CtaImage from '../assets/CtaImage.jpg';
 const samplePostImage = '/images/sample.jpg';
+const userImage = '/images/user.png'
 
 const images = {
     logo,
@@ -12,6 +13,7 @@ const images = {
     PostProfileImage,
     CtaImage,
     samplePostImage,
+    userImage
 }
 
 export default images;
