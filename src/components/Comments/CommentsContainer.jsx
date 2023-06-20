285f424a90160559e6fd6245299d3f43b53ac3fd
levilu800b <levilu800b@gmail.com>
2023-06-20 12:21:42 +0100

    delete comment

--- src/components/Comments/CommentsContainer.jsx
+++ src/components/Comments/CommentsContainer.jsx
@@ -3,11 +3,20 @@ import Comment from './Comment';
 
 import CommentForm from './CommentForm';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { createNewComment, updateComment } from '../../services/index/comments';
+import {
+	createNewComment,
+	deleteComment,
+	updateComment,
+} from '../../services/index/comments';
 import { useSelector } from 'react-redux';
 import { toast } from 'react-hot-toast';
 
-const CommentsContainer = ({ className, loggedInUserId, comments, postSlug }) => {
+const CommentsContainer = ({
+	className,
+	loggedInUserId,
+	comments,
+	postSlug,
+}) => {
 	const queryClient = useQueryClient();
 	const userState = useSelector((state) => state.user);
 	const [affectedComment, setAffectedComment] = useState(null);
@@ -18,30 +27,43 @@ const CommentsContainer = ({ className, loggedInUserId, comments, postSlug }) =>
 				return createNewComment({ token, desc, slug, parent, replyOnUser });
 			},
 			onSuccess: () => {
-				toast.success("Your comment is sent successfully, it will be visible after the confirmation of the Admin")
+				toast.success(
+					'Your comment is sent successfully, it will be visible after the confirmation of the Admin',
+				);
 			},
 			onError: (error) => {
-				toast.error(error.message)
+				toast.error(error.message);
 				console.log(error);
-			}
+			},
 		});
 
-		const { mutate: mutateUpdateComment } =
-			useMutation({
-				mutationFn: ({ token, desc, commentId }) => {
-					return updateComment({ token, desc, commentId });
-				},
-				onSuccess: () => {
-					toast.success(
-						'Your comment is updated successfully',
-					);
-					queryClient.invalidateQueries(['blog', postSlug])
-				},
-				onError: (error) => {
-					toast.error(error.message);
-					console.log(error);
-				},
-			});
+	const { mutate: mutateUpdateComment } = useMutation({
+		mutationFn: ({ token, desc, commentId }) => {
+			return updateComment({ token, desc, commentId });
+		},
+		onSuccess: () => {
+			toast.success('Your comment is updated successfully');
+			queryClient.invalidateQueries(['blog', postSlug]);
+		},
+		onError: (error) => {
+			toast.error(error.message);
+			console.log(error);
+		},
+	});
+
+	const { mutate: mutateDeleteComment } = useMutation({
+		mutationFn: ({ token, desc, commentId }) => {
+			return deleteComment({ token, commentId });
+		},
+		onSuccess: () => {
+			toast.success('Your comment is deleted successfully');
+			queryClient.invalidateQueries(['blog', postSlug]);
+		},
+		onError: (error) => {
+			toast.error(error.message);
+			console.log(error);
+		},
+	});
 
 	const addCommentHandler = (value, parent = null, replyOnUser = null) => {
 		mutateNewComment({
@@ -58,12 +80,14 @@ const CommentsContainer = ({ className, loggedInUserId, comments, postSlug }) =>
 		mutateUpdateComment({
 			token: userState.userInfo.token,
 			desc: value,
-			commentId
-		})
+			commentId,
+		});
 		setAffectedComment(null);
 	};
 
-	const deleteCommentHandler = (commentId) => {};
+	const deleteCommentHandler = (commentId) => {
+		mutateDeleteComment({ token: userState.userInfo.token, commentId });
+	};
 
 	return (
 		<div className={`${className}`}>
--- src/services/index/comments.js
+++ src/services/index/comments.js
@@ -58,3 +58,23 @@ export const updateComment = async ({
 		throw new Error(error.message);
 	}
 };
+
+export const deleteComment = async ({ token, commentId }) => {
+	try {
+		const config = {
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		};
+
+		const { data } = await axios.delete(
+			`/api/comments/${commentId}`,
+			config,
+		);
+		return data;
+	} catch (error) {
+		if (error.response && error.response.data.message)
+			throw new Error(error.response.data.message);
+		throw new Error(error.message);
+	}
+};
