f88dd7459cbb7d98a06fb24adf32f5750f6b31a7
levilu800b <levilu800b@gmail.com>
2023-07-01 09:50:29 +0100

    dashboard start

---
+++ src/pages/admin/AdminLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AdminLayout = () => {
+  return (
+    <div className='flex flex-col h-screen lg:flex-row'>
+        
+    </div>
+  )
+}
+
+export default AdminLayout
