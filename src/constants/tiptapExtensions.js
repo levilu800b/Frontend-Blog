1b6932325a527d8874282b21a83157a391d5eba1
levilu800b <levilu800b@gmail.com>
2024-03-17 16:44:28 +0000

    bug fix

--- src/constants/tiptapExtensions.js
+++ src/constants/tiptapExtensions.js
@@ -2,8 +2,10 @@ import { Color } from '@tiptap/extension-color';
 import ListItem from '@tiptap/extension-list-item';
 import TextStyle from '@tiptap/extension-text-style';
 import StarterKit from '@tiptap/starter-kit';
+import Dropcursor from '@tiptap/extension-dropcursor';
+import Image from '@tiptap/extension-image';
 import CodeBlockLowlight from '@tiptap/extension-code-block-lowlight';
-import { lowlight } from 'lowlight';
+import { lowlight } from 'lowlight/lib/common';
 import css from 'highlight.js/lib/languages/css';
 import js from 'highlight.js/lib/languages/javascript';
 import ts from 'highlight.js/lib/languages/typescript';
@@ -30,4 +32,6 @@ export const extensions = [
   CodeBlockLowlight.configure({
     lowlight,
   }),
+  Dropcursor,
+  Image,
 ];
