dc67a2f12886d8ba7c8956cca552fb32acd204e7
levilu800b <levilu800b@gmail.com>
2024-05-14 22:33:46 +0100

    updated for deployment

--- src/constants/stables.js
+++ src/constants/stables.js
@@ -1,4 +1,5 @@
-const UPLOAD_FOLDER_BASE_URL = "http://localhost:5000/uploads/"
+const UPLOAD_FOLDER_BASE_URL =
+  'https://backend-blog-1op1.onrender.com/uploads/';
 
 const stables = {
     UPLOAD_FOLDER_BASE_URL
