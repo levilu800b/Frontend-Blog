e7afb66e993977a01772157341fccd1463961b00
levilu800b <levilu800b@gmail.com>
2024-11-10 13:53:39 +0000

    prep for deployment

--- src/constants/stables.js
+++ src/constants/stables.js
@@ -1,7 +1,9 @@
-const UPLOAD_FOLDER_BASE_URL = "http://localhost:5000/uploads/"
+const UPLOAD_FOLDER_BASE_URL = `${process.env.REACT_APP_API_BASE_URL}/uploads/`;
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 const stables = {
-    UPLOAD_FOLDER_BASE_URL
+    UPLOAD_FOLDER_BASE_URL,
+    API_BASE_URL
 }
 
 export default stables;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
 import store from './store';
+import { stables } from './constants';
+
+axios.defaults.baseURL = stables.API_BASE_URL;
 
 const queryClient = new QueryClient({
   defaultOptions: {
